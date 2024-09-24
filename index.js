a93aea0497adf3f757b4b11ac7650fb4d68799e7
Aravindhan-K97 <aravindhan2825@gmail.com>
2024-09-25 03:06:01 +0530

    Url added

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({ 
-  origin: 'http://localhost:5173',
+  origin: 'https://tangerine-tartufo-348612.netlify.app',
   credentials: true 
 }));
 
